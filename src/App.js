c6839582cea74e82971f2a406881e86498bf5ab5
Sapphire Paston <sapphire.paston@gmail.com>
2021-10-27 12:25:10 +0100

    Set up basic Homepage markup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Homepage from './components/Homepage'
+
+
 
 
 const App = () => {
 
- 
-  
+
+
   return (
     <>
-      <h1>Project 2</h1>
+      <Homepage />
     </>
   )
 }
 
 export default App
+
+
+// To access and use the Listen Notes API key, use:
+// process.env.REACT_APP_ListenNotesKey
---
+++ src/components/Homepage.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Homepage = () => {
+
+  return (
+    <>
+      <section className="hero columns is-fullheight-with-navbar">
+        <section className="section column is-one-third">
+          <div className="container">
+
+          </div>
+        </section>
+        <section className="section column is-two-thirds">
+          <div className="container">
+
+          </div>
+        </section>
+      </section>
+
+    </>
+  )
+
+}
+
+export default Homepage
+
+// Homepage plan
+
+// Make separate NavBar component (child of Homepage) and import in
+// Add some basic markup for Filter Panel and Discover By Genre
+
+
+// Filter Panel
+
+// Set up pieces of state using useState hook
+// Add event listeners to dropdown inputs in the Filter Panel Form
+// Add event listener to the Filter Panel Form
+// Create handleChange function
+// Create handleClick function
+
+
+// Discover By Genre
