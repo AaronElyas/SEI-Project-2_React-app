6fae36b10439f3003459c80e6d362e4c3d63eb07
Sapphire Paston <sapphire.paston@gmail.com>
2021-10-28 20:05:35 +0100

    All emoji Genre icons added

--- src/components/ShowGenres.js
+++ src/components/ShowGenres.js
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from 'react'
-import { useHistory, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 const ShowGenres = () => {
 
   const [genreData, setGenreData] = useState([])
-  const history = useHistory()
 
-  const imageArray = ['💰', '🏘', '🎥', 'assets/tech.jpg', 'assets/crime.jpg', 'assets/arts.jpg', '/assets/business.jpg',
-    '/assets/comedy.jpg', '/assets/education.jpg', '/assets/fiction.jpg', '/assets/gov.jpg', '/assets/fitness.jpg', '/assets/history.jpg', '/assets/fam.jpg',
-    '/assets/leisure.jpg', '/assets/music.jpg', '/assets/news.jpg', '/assets/religion.jpg', '/assets/science.jpg', '/assets/culture.jpg', '/assets/sports.jpg']
+  const emojisObject = {
+    144: '💵',
+    151: '🏘',
+    68: '🎥',
+    127: '💻',
+    135: '🔎',
+    134: '🎼',
+    99: '📰',
+    69: '🙏',
+    107: '🔬',
+    122: '🌍',
+    77: '🏀',
+    93: '💼',
+    133: '🎭',
+    111: '🎓',
+    168: '📚',
+    117: '⚖️',
+    88: '❤️‍🩹',
+    125: '🏛',
+    132: '🍼',
+    82: '🛀',
+    100: '🎨',
+  }
+
 
   useEffect(() => {
     const getData = async () => {
@@ -17,8 +37,8 @@ const ShowGenres = () => {
         const { data: { genres } } = await axios.get('https://listen-api.listennotes.com/api/v2/genres?top_level_only=1', {
           headers: { 'X-ListenAPI-Key': process.env.REACT_APP_ListenNotesKey },
         })
-        setGenreData(genres)
         console.log(genres)
+        setGenreData(genres)
       } catch {
         console.warn('failure to get API data')
       }
@@ -33,14 +53,15 @@ const ShowGenres = () => {
 
 
   return (
-    genreData.map((item, index) => {
+    genreData.map((item) => {
       return (
+        // <div key={item.id} className="columns is-multiline">
         <div key={item.id} className="column is-one-third-desktop is-one-half-tablet">
           <Link to={{ pathname: '/genrerecommendations', state: item.id }}>
             <div className="card">
               <div className="card-image">
                 <span role="img" aria-label="logo" className="title is-flex is-justify-content-center">
-                  <p className='emoji'>{imageArray[index]}</p>
+                  <p className='emoji'>{emojisObject[item.id]}</p>
                 </span>
               </div>
               <div className='card-footer'>
@@ -49,6 +70,7 @@ const ShowGenres = () => {
             </div>
           </Link >
         </div>
+        // </div>
       )
     })
   )
