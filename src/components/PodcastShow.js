a60bf74a721f17f033431edd4ca081b656f6e3bc
Sapphire Paston <sapphire.paston@gmail.com>
2021-10-28 20:50:34 +0100

    API request and markup (with layout and style) for PodcastShow added

--- src/components/PodcastShow.js
+++ src/components/PodcastShow.js
@@ -1,36 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+// import PodRecommend from './PodRecommend'
 
-const PodcastShow = ({ data }) => {
+const PodcastShow = () => {
+
+  const { id } = useParams()
+  const [singlePod, setSinglePod] = useState([])
+  // const [flip, setFlip] = useState(false)
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get(`https://listen-api.listennotes.com/api/v2/podcasts/${id}?next_episode_pub_date=1479154463000&sort=recent_first`, {
+        headers: { 'X-ListenAPI-Key': process.env.REACT_APP_ListenNotesKey },
+      })
+      setSinglePod(data)
+    }
+    getData()
+  }, [])
 
 
 
   return (
-    <div className="card">
-      <div className="card-header">
-        <div className="card-header-title title">Podcast Title</div>
+
+    <section className="section">
+      <div className="container">
+        <div>
+          <h2 className="title has-text-centered">{singlePod.title}</h2>
+          <hr />
+          <div className="columns">
+            <div className="column is-half">
+              <figure className="image is-1by1">
+                <img src={singlePod.image} alt={singlePod.title}></img>
+              </figure>
+            </div>
+            <div className="column is-half">
+              <h4 className="title is-4">🎙 {singlePod.publisher}</h4>
+              <p className="is-size-5	has-text-weight-bold">{singlePod.country}</p>
+              <p className="is-size-6">{singlePod.language} language</p>
+              <hr />
+              <p>{singlePod.description}</p>
+              <hr />
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="card-image">
-        <figure className="image image-is-1by1">
-          {/* <img src={podcast.image} alt={podcast.title}></img> */}
-        </figure>
-      </div>
-      <div className="card-content">
-        <div>
-          <h3 className="has-text-weight-bold subtitle">Publisher</h3>
-        </div>
-        <div>
-          <p className="mt-4">Description</p>
-        </div>
-        <div>
-          <p className="has-text-weight-bold">Country</p>
-        </div>
-        <div>
-          <p className="has-text-weight-bold">Language</p>
-        </div>
-      </div>
-    </div>
+    </section >
+
   )
 
 }
 
 export default PodcastShow
+
+// <div className='is-flex is-flex-direction-column is-align-items-center'>
+//   <div className="card">
+//     <div className="card-header">
+//       <div className="card-header-title title">{singlePod.title}</div>
+//     </div>
+//     <div className="card-image">
+//       <figure className="image is-1by1">
+//         <img src={singlePod.image} alt={singlePod.title}></img>
+//       </figure>
+//     </div>
+//     <div className="card-content">
+//       <div>
+//         <h3 className="has-text-weight-bold subtitle">{singlePod.publisher}</h3>
+//       </div>
+//       <div>
+//         <p className="mt-4">{singlePod.description}</p>
+//       </div>
+//       <div>
+//         <p className="has-text-weight-bold">{singlePod.country}</p>
+//       </div>
+//       <div>
+//         <p className="has-text-weight-bold">{singlePod.language}</p>
+//       </div>
+//     </div>
+//   </div>
+{/* <button onClick={() => setFlip(true)} className='has-text-black button is-warning is-rounded mt-4'>Show more like this</button>
+    {flip && <PodRecommend id={singlePod.id} />} */}
+{/* </div> */ }
